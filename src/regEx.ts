789b24d63d1bc5f504ffb690c13bad85bb50ae7c
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2024-08-14 19:45:24 +0200

    regeg for unacuahg functions

--- src/regEx.ts
+++ src/regEx.ts
@@ -6,7 +6,7 @@ export function getFunctionDefinitionRegex(languageId: string): RegExp {
     case "typescript":
     case "javascriptreact":
     case "typescriptreact":
-      return /(?:function\s+(\w+)|(?:const|let|var)\s+(\w+)\s*=\s*(?:function|\([^)]*\)\s*=>|async\s*(?:function|\([^)]*\)\s*=>))|(?:const|let|var)\s+(\w+)\s*=\s*\([^)]*\)\s*=>|\b(\w+)\s*:\s*(?:function|\([^)]*\)\s*=>)|(?:class\s+(\w+)|const\s+(\w+)\s*=\s*class)|(?:const|let|var)\s+(\w+)\s*=\s*React\.(?:memo|forwardRef)\()/g;
+      return /(?<!import\s*{[^}]*)\b(?:function\s+(\w+)|(?:const|let|var)\s+(\w+)\s*=\s*(?:function|\([^)]*\)\s*=>|async\s*(?:function|\([^)]*\)\s*=>))|(?:const|let|var)\s+(\w+)\s*=\s*\([^)]*\)\s*=>|\b(\w+)\s*:\s*(?:function|\([^)]*\)\s*=>)|(?:class\s+(\w+)|const\s+(\w+)\s*=\s*class)|(?:const|let|var)\s+(\w+)\s*=\s*React\.(?:memo|forwardRef)\()|(?:export\s+(?:async\s+)?function\s+(\w+))/g;
     default:
       return /(?:)/g; // Empty regex for unsupported file types
   }
